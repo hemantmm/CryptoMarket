ff1bd96ec8b407b23bbe113e7f0a15b37c625433
Hemant M Mehta <hemant29mehta@gmail.com>
2026-08-18 18:44:12 +0530

    fix: removed unused code
    Signed-off-by: Hemant M Mehta <hemant29mehta@gmail.com>

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { TrendingDown, TrendingUp } from "../icons/icons";
 import { currencyFormat } from "../utils";
 // import Trending from "./Trending";
